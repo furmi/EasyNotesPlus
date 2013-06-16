9d79e13a75951eec4c1bf5718af3f9fd63eba157
furmi <tomesman@gmail.com>
2013-06-16 19:02:19 +0200

    Ajout du TagManager porté sur Qt

---
+++ Logic/TagManager.cpp
@@ -0,0 +1,163 @@
+//
+//  TagManager.cpp
+//  EasyNotePlus_thomas
+//
+//  Created by Thomas Fuhrmann on 16/06/13.
+//  Copyright (c) 2013 UTC. All rights reserved.
+//
+
+#include "TagManager.h"
+
+using namespace EasyNote;
+
+namespace EasyNote
+{
+    // Initialisation de l'attribut statique
+
+	TagManager* TagManager::instance = 0;
+
+		// CONSTRUCTEURS/DESTRUCTEURS
+
+	TagManager::TagManager():assoc(),tags(){}
+
+	TagManager::~TagManager ()
+	{
+		// Destruction du TagManager
+
+		releaseInstance();
+	}
+
+    // MÉTHODES STATIQUES
+
+	TagManager* TagManager::getInstance()
+	{
+		if (instance == 0)
+			instance = new TagManager;
+
+		return TagManager::instance;
+	}
+
+	void TagManager::releaseInstance()
+	{
+		if (instance != 0)
+			delete instance;
+	}
+
+	// Gestion des tags
+    void TagManager::addTag(Note* n, const QString& tag)
+    {
+        map<Note*,set<QString> >::iterator itMap;
+        itMap = assoc.find(n);  //on recherche si la note existe
+        if(itMap == assoc.end())   //la note n'existe pas, on la rajoute
+        {
+            set<QString> tmp;
+            tmp.insert(tag);
+            assoc.insert(pair<Note*,set<QString> >(n,tmp));
+            tags.insert(tag);   //on ajoute le Tag à la liste des Tags du TagManager
+        }
+        else    //la note existe, on cherche si le tag existe
+        {
+            set<QString>::iterator itTag;
+            itTag = itMap->second.find(tag);   //on recherche le tag dans la note correspondante
+            if(itTag == itMap->second.end())   //le tag n'existe pas encore
+            {
+                itMap->second.insert(tag);     //on ajoute le tag
+                tags.insert(tag);   //on ajoute le Tag à la liste des Tags du TagManager
+            }
+        }
+    }
+
+    void TagManager::removeTag(Note* n, const QString& tag)
+    {
+        map<Note*,set<QString> >::iterator itMap;
+        itMap = assoc.find(n);  //on recherche si la note existe
+        if(itMap != assoc.end())   //la note n'existe pas, on la rajoute
+        {
+            set<QString>::iterator itTag;
+            itTag = itMap->second.find(tag);   //on recherche le tag dans la note correspondante
+            if(itTag != itMap->second.end())   //le tag n'existe pas encore
+            {
+                itMap->second.erase(tag);     //on ajoute le tag
+            }
+        }
+    }
+
+    void TagManager::removeTag(const QString& tag)
+    {
+        map<Note*,set<QString> >::iterator itMap;
+        for(itMap = assoc.begin(); itMap != assoc.end(); itMap++)   //on parvourt la liste de Note et on supprime le tag
+            removeTag((*itMap).first, tag);
+        tags.erase(tag);    //on supprime le tag de la liste de tag du TagManager
+    }
+
+    //Recherche
+    set<Note*> TagManager::searchNote(set<QString> tags)  //OU entre les tags
+    {
+        map<Note*,set<QString> >::iterator itMap; //parcourt des notes
+        set<Note*> ret; //set de retour avec la liste des notes
+        set<QString>::iterator itTag;    //permet la recherche dans le set
+        set<QString>::iterator itParam;  //permet de parcourir le set de tags envoyé en paramètre
+        for(itMap = assoc.begin(); itMap != assoc.end(); itMap++)
+        {
+            for(itParam = tags.begin(); itParam != tags.end(); itParam++)
+            {
+                itTag = itMap->second.find(*itParam);
+                if(itTag != itMap->second.end())   //le tag existe
+                {
+                    ret.insert((*itMap).first); //on ajoute la note à la liste de retour
+                }
+            }
+        }
+        return ret;
+    }
+
+    set<Note*> TagManager::filterNote(set<QString> tags)  //ET entre les tags
+    /* Fonctionnement :
+    Pour chaque Tag de tags, on recherche l'ensemble des notes correspondantes (via serachNote).
+    On travail sur 2 set : old et current. Pour chaque pas de la boucle, on a dans current le set de note
+    qui correspond au tag courant. Old est une mémoire du pas d'avant.
+    A chaque pas on fait l'intersection entre les éléments de old et de current et on sauvegarde
+    le résultat dans old. Cela permet, une fois tous les tags parcouru, de renvoyer le set de note
+    où on aura un ET entre les tags.
+    */
+    {
+        set<Note*> tmp; //set temporaire pour les opérations d'intersection
+        set<Note*> old; //set désignant la mémoire de parcourt
+        set<Note*>::iterator itold; //iterator sur old
+        set<Note*> current; //set contenant la liste actuelle de note
+        set<Note*>::iterator itcurrent; //iterator sur current
+        set<QString>::iterator ittags;   //iterator sur tags passé en paramètre
+        set<QString> param;  //
+
+        ittags = tags.begin();
+        param.insert(*ittags);
+        cout<<"Parametre (1er tag) "<<(*(param.begin()))<<endl;
+        old = this->searchNote(param);  //liste des note qui ont le premier tag
+        if (tags.size() == 1)
+            return old; //si on a qu'un seul tag, la méthode renvoit la liste du tag
+        else
+        {
+            ittags++;   //on commence au deuxième élément comme on a déjà récupéré le premier
+            for(ittags; ittags != tags.end(); ittags++)  //on parcourt la liste des tags
+            {
+                //on récupère la liste de note associé au tag courant
+                param.clear();
+                param.insert(*ittags);
+                current = this->searchNote(param);
+                //on fait l'intersection entre current et old
+                for(itold = old.begin(); itold != old.end(); itold++)   //boucle sur old
+                {
+                    for(itcurrent = current.begin(); itcurrent != current.end(); itcurrent++)   //boucle sur current
+                    {
+                        if((*itold) == (*itcurrent))   //la note se trouve dans les deux sets
+                        {
+                            tmp.insert((*itold)); //on ajoute la note à la liste de retour
+                        }
+                    }
+                }
+                old = tmp;  //on met à jour la "mémoire" de parcourt qui coutient l'intersection des deux set
+            }
+            return old;
+        }
+    }
+}//fin du namespace
---
+++ Logic/TagManager.h
@@ -0,0 +1,72 @@
+//
+//  TagManager.h
+//  EasyNotePlus_thomas
+//
+//  Created by Thomas Fuhrmann on 16/06/13.
+//  Copyright (c) 2013 UTC. All rights reserved.
+//
+
+#ifndef EasyNotePlus_thomas_TagManager_h
+#define EasyNotePlus_thomas_TagManager_h
+#include <map>
+#include <set>
+#include <QString>
+#include "Note.h"
+
+/** \class TagManager
+*
+*   Classe permet la gestion des Tags.
+*/
+
+using namespace std;
+
+// Références
+
+namespace EasyNote
+{
+    class TagManager
+    /**
+	*	\class TagManager : \brief Classe qui permet de gérer les tags.
+	*/
+    {
+    // Attributes
+
+        static TagManager* instance;
+        map<Note*,set<QString> > assoc; //map<ID_Note,vector<tags>> liste des associations
+        set<QString> tags;   //liste des tags
+
+
+    //METHODES
+
+        // CONSTRUCTEURS / DESTRUCTEURS
+        TagManager();
+        TagManager(const TagManager& tm);
+        TagManager operator=(const TagManager& tm);
+        ~TagManager();
+
+        // SINGLETON
+    public:
+        /**
+        *	\fn getInstance: \brief retourne l'instance
+        */
+
+        static TagManager* getInstance ();
+
+        /**
+         * \fn releaseInstance: \brief Supprimme le singleton
+         */
+
+        static void releaseInstance();
+
+        // Gestion des tags
+        void addTag(Note* n, const QString& tag); //permet d'ajouter un tag à une note
+        void removeTag(Note* n, const QString& tag);  //permet de supprimer une association tag/note
+        void removeTag(const QString& tag); //permet de supprimer un tag (supprime toutes les associations tag/note
+
+        //Recherche
+        set<Note*> searchNote(set<QString>);  //search = OU entre les tags
+        set<Note*> filterNote(set<QString>);   //filtre = ET entre les tags
+    };
+}
+
+#endif
